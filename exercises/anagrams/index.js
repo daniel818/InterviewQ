095e6cc3356d6f57a36f4609f0d40b6adf19521d
daniel818 <avital.daniel@gmail.com>
2017-12-10 15:56:52 +0200

    finisjing string

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -1,5 +1,5 @@
 // --- Directions
-// Check to see if two provided strings are anagrams of eachother.
+// Check to see if two provided strings are anagrams of each other.
 // One string is an anagram of another if it uses the same characters
 // in the same quantity. Only consider characters, not spaces
 // or punctuation.  Consider capital letters to be the same as lower case
@@ -8,6 +8,42 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// function anagrams(stringA, stringB) {
+//   const aChar = buildCharMap(stringA);
+//   const bChar = buildCharMap(stringB);
+//
+//   if (Object.keys(aChar).length !== Object.keys(bChar).length) {
+//     return false;
+//   }
+//   for (let char in aChar) {
+//     if (aChar[char] !== bChar[char]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// function buildCharMap(str) {
+//   const charMap = {};
+//
+//   for (let char of str.replace(/[^\w]/g, "").toLocaleLowerCase()) {
+//     charMap[char] = charMap[char] + 1 || 1;
+//   }
+//   return charMap;
+// }
+
+//second solution
+function anagrams(stringA, stringB) {
+  return cleanStr(stringA) === cleanStr(stringB)
+}
+
+function cleanStr(str) {
+  return str
+    .replace(/[^\w]/g, "")
+    .toLocaleLowerCase()
+    .split("")
+    .sort()
+    .join("");
+}
 
 module.exports = anagrams;
--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,19 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n, row = 0, stair = "") {
+  if (n === row) {
+  }
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+  if (stair.length <= row) {
+    stair += "#";
+  } else {
+    stair += " ";
+  }
+  steps(n, row, stair);
+}
 
 module.exports = steps;
